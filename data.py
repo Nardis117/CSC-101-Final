bfc70be5bfcb5a5e75e1e7813fa0cd5832028e31
Ethan Huynh <ehuynh25@calpoly.edu>
2026-03-08 19:30:10 -0700

    class def

---
+++ data.py
@@ -0,0 +1,10 @@
+class PlaceAirQuality:
+    def __init__(self, place_id, place_name, place_type, aqi_week, pm25_week):
+        self.place_id = place_id
+        self.place_name = place_name
+        self.place_type = place_type
+        self.aqi_week = aqi_week
+        self.pm25_week = pm25_week
+
+
+
