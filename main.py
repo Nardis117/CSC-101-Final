6806bf75e5455f5efe5431bb4b4e1a42d9a4033f
Ethan Huynh <ehuynh25@calpoly.edu>
2026-03-08 22:06:43 -0700

    main.py suedo code suggestions

---
+++ main.py
@@ -0,0 +1,31 @@
+def main():
+    #places = variable to load places from text or file reading
+    #set empty dict for average aqi and emissions per capita
+
+    #set total aqi and pm25 = 0
+
+    #for loop for each place/area:
+        # set avg_aqi = calculation in data.py
+        # same for avg_pm25
+
+        #max-min values = find_aqi_extremes()
+
+
+        #total_emissions = total_emissions()
+        #emissions_per_capita =
+        #maybe add top emissions too?
+
+        #any other variables
+
+        #print specifics
+        # "Average AQI:
+        # AVERAGE PM2.5:"
+        # etc
+
+    #overall calculations
+    #print overall
+
+if __name__ == "__main__":
+    main()
+
+
